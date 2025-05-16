ef9b22579067dfdd89301d19e51bc1d2131d56ff
TheJustRusik <7thejustrusik7@gmail.com>
2025-05-17 00:49:46 +0500

    fixed header

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,22 @@ import Header from "@/components/Header.vue";
 </script>
 
 <template>
-  <Header/>
-
+  <div class="fixed z-1 w-full backdrop-blur-md">
+  <Header />
+  </div>
+  <main class="pt-custom">
   <RouterView />
+  </main>
 </template>
+
+<style scoped>
+.pt-custom {
+  padding-top: 7rem;
+}
+
+@media (min-width: 768px) {
+  .pt-custom {
+    padding-top: 5rem;
+  }
+}
+</style>
